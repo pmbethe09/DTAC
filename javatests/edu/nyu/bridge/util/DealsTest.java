cb9ca75a3eeb163d9673cad1db9a3f1530a3cfe8
Paul Bethe <pmbethe09@users.noreply.github.com>
2017-01-02 22:06:42 -0500

    fix broken DealsTest

--- javatests/edu/nyu/bridge/util/DealsTest.java
+++ javatests/edu/nyu/bridge/util/DealsTest.java
@@ -2,9 +2,12 @@ package edu.nyu.bridge.util;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Test;
+
 import edu.nyu.bridge.gen.Bridge.Vulnerability;
 
 public class DealsTest {
+  @Test
   public void testVuls() {
     assertEquals(Vulnerability.NONE, Deals.getVul(8));
     assertEquals(Vulnerability.BOTH, Deals.getVul(4));
