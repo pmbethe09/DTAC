f884e9a2d75d1d18658cb3602822a34ef2617a70
Paul Bethe <pmbethe09@users.noreply.github.com>
2018-10-12 16:00:38 -0400

    add easier factory method

--- java/edu/nyu/bridge/scoring/ContractScore.java
+++ java/edu/nyu/bridge/scoring/ContractScore.java
@@ -1,6 +1,8 @@
 package edu.nyu.bridge.scoring;
 
 import com.google.auto.value.AutoValue;
+import edu.nyu.bridge.gen.Bridge;
+import edu.nyu.bridge.util.Calls;
 import edu.nyu.bridge.util.Contract;
 
 /** Wraps up a {@link Contract} and its {@link Score}. */
@@ -8,6 +10,10 @@ import edu.nyu.bridge.util.Contract;
 public abstract class ContractScore {
   public static ContractScore of(Contract contract, Score score) {
     return new AutoValue_ContractScore(contract, score);
+  }
+
+  public static ContractScore of(Bridge.Direction direction, Bridge.Bid bid, int score) {
+    return of(Contract.of(Calls.bid2Call(bid), direction), Score.of(direction, score));
   }
 
   public abstract Contract contract();
